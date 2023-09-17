a82a13db9c895e56c57c001bbb4c8651373ff11a
moawezz <moawez.de@gmail.com>
2023-09-17 12:11:06 +0330

    create similar word table as linked table

--- models/model.py
+++ models/model.py
@@ -1,4 +1,4 @@
-from peewee import Model, SqliteDatabase, CharField, PrimaryKeyField
+from peewee import Model, SqliteDatabase, CharField, PrimaryKeyField, ForeignKeyField, Cas
 
 
 db = SqliteDatabase()
@@ -7,6 +7,13 @@ class WordBook(Model):
     full_word = CharField()
     full_word_with_symbols = CharField()
     definition = CharField()
+
+    class Meta:
+        database = db
+
+class SimilarWord(Model):
+    main_word = ForeignKeyField(model=WordBook, backref="similar_words", on_delete="CASCADE")
+    similar_title = CharField()
 
     class Meta:
         database = db
